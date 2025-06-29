a84e2f14b7bbd5e61b3abab0e01638fda1b96be0
ifurusato <ichiro.furusato@gmail.com>
2025-06-29 17:00:42 +1200

    removed support for UART 4; replaced with support for SD card

--- WEACT_H562RGT6/mpconfigboard.h
+++ WEACT_H562RGT6/mpconfigboard.h
@@ -1,3 +1,4 @@
+
 #define MICROPY_HW_BOARD_NAME       "WeAct STM32H562RGT6"
 #define MICROPY_HW_MCU_NAME         "STM32H562RGT6"
 
@@ -44,8 +45,9 @@
 #define MICROPY_HW_UART2_RX                 (pyb_pin_UART2_RX)
 #define MICROPY_HW_UART3_TX                 (pyb_pin_UART3_TX)
 #define MICROPY_HW_UART3_RX                 (pyb_pin_UART3_RX)
-#define MICROPY_HW_UART4_TX                 (pyb_pin_UART4_TX)
-#define MICROPY_HW_UART4_RX                 (pyb_pin_UART4_RX)
+// UART4 removed to avoid SD card conflict
+// #define MICROPY_HW_UART4_TX              (pyb_pin_UART4_TX)
+// #define MICROPY_HW_UART4_RX              (pyb_pin_UART4_RX)
 #define MICROPY_HW_UART_REPL                PYB_UART_1
 #define MICROPY_HW_UART_REPL_BAUD           115200
 
@@ -66,3 +68,39 @@
 
 // USB config (FS on PA11/PA12 is typical)
 #define MICROPY_HW_USB_FS                   (1)
+
+// === SD card (SDMMC/SDIO) support ===
+#define MICROPY_HW_ENABLE_SDCARD            (1)
+// No card detect pin defined, as socket does not provide one
+// #define MICROPY_HW_SDCARD_DETECT_PIN        (pin_Xx)
+// #define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+// #define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
+// SDMMC pins are configured via pins.csv:
+// SD_D0  PC8
+// SD_D1  PC9
+// SD_D2  PC10
+// SD_D3  PC11
+// SD_CMD PD2
+// SD_CK  PC12
+
+/* --- SDMMC1 AF PATCH BEGIN --- */
+// SDMMC1 AF defines (patch inserted by build-weactstmh562.sh)
+#ifndef STATIC_AF_SDMMC1_CK
+#define STATIC_AF_SDMMC1_CK(x)  (12)
+#endif
+#ifndef STATIC_AF_SDMMC1_CMD
+#define STATIC_AF_SDMMC1_CMD(x) (12)
+#endif
+#ifndef STATIC_AF_SDMMC1_D0
+#define STATIC_AF_SDMMC1_D0(x)  (12)
+#endif
+#ifndef STATIC_AF_SDMMC1_D1
+#define STATIC_AF_SDMMC1_D1(x)  (12)
+#endif
+#ifndef STATIC_AF_SDMMC1_D2
+#define STATIC_AF_SDMMC1_D2(x)  (12)
+#endif
+#ifndef STATIC_AF_SDMMC1_D3
+#define STATIC_AF_SDMMC1_D3(x)  (12)
+#endif
+/* --- SDMMC1 AF PATCH END --- */
